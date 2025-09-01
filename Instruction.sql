245592cdfee68d714134758f6e06a32fef949851
Amit Baviskar <153518736+Amit-Baviskar@users.noreply.github.com>
2025-09-01 19:41:07 +0530

    Add files via upload

---
+++ Instruction.sql
@@ -0,0 +1,37 @@
+
+-- Windows
+virtualenv venv
+venv\Scripts\activate
+
+-- MAC
+mkdir course
+cd course
+virtualenv venv
+. venv/bin/activate
+
+-- install dbt snowflake
+pip install dbt-snowflake==1.9.0
+
+-- create dbt profile
+-- mac
+mkdir ~/.dbt
+
+-- windows 
+mkdir %userprofile%\.dbt
+
+#initiate dbt project 
+dbt init <projectname>
+
+-- Add staging tables -> all will be views
+-- Update dbt_project.yaml file to support of tables
+dim:
+  +materialized: table
+--   +schema: dim
+-- fct:
+--   +materialized: incremental
+--   +schema: fct
+-- mart:
+--   +materialized: table
+--   +schema: mart
+-- snapshots:
+--   +schema: snapshots
